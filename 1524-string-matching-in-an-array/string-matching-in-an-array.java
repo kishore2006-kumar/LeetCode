80739445d0d5e0c1811dfa0a645117a16f61ae80
kishore2006-kumar <kishorekumartr006@gmail.com>
2026-08-08 13:43:27 +0530

    Time: 4 ms (96.31%) | Memory: 43.8 MB (30.26%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<String> stringMatching(String[] a) {
         List<String> list=new ArrayList<>();
-        Set<String> set=new HashSet<>();
         for(int i=0; i<a.length; i++) {
             String s = a[i];
             for(int j=0; j<a.length; j++) {
                 String str = a[j];
-                if(j != i && str.indexOf(s) != -1) set.add(s);
+                if(j != i && str.indexOf(s) != -1 && !list.contains(s)) list.add(s);
             }
         }
-        // for(String x : set) {
-        //     list.add(set);
-        // }
-        return new ArrayList<>(set);
+        return list;
     }
 }
