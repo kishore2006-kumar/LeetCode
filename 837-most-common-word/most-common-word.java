3bb51b3407362b8e6b6fdec3f59590b556011cc4
kishore2006-kumar <kishorekumartr006@gmail.com>
2026-08-12 16:29:40 +0530

    Time: 48 ms (5.48%) | Memory: 47.6 MB (5.05%) - LeetSync

--- 837-most-common-word/most-common-word.java
+++ 837-most-common-word/most-common-word.java
@@ -10,17 +10,45 @@ class Solution {
         String[] s = para.toLowerCase().trim().split("\\s+");
         System.out.print(Arrays.toString(s));
 
-        HashMap<String, Integer> map=new HashMap<>();
-        int max = 0;
+        HashSet<String> set=new HashSet<>();
         for(String str : s) {
-            int count = map.getOrDefault(str, 0) + 1;
-            map.put(str, count);
+            set.add(str);
+        }
 
-            if(count > max) {
-                max = count;
-                para = str;
+        System.out.print(set);
+
+        List<String> list=new ArrayList<>(set);
+
+        int[] freq=new int[set.size()];
+        for(int j=0; j<list.size(); j++) {
+            String str = list.get(j);
+            for(int i=0; i<s.length; i++) {
+                if(s[i].equals(str)) freq[j]++;
             }
         }
-        return para;
+
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for(int i=0; i<freq.length; i++) {
+            if(freq[i] > max) {
+                max = freq[i];
+                index = i;
+            }
+        }
+
+        return index == -1 ? new String() : list.get(index);
+
+        // HashMap<String, Integer> map=new HashMap<>();
+        // int max = 0;
+        // for(String str : s) {
+        //     int count = map.getOrDefault(str, 0) + 1;
+        //     map.put(str, count);
+
+        //     if(count > max) {
+        //         max = count;
+        //         para = str;
+        //     }
+        // }
+        // return para;
     }
 }
