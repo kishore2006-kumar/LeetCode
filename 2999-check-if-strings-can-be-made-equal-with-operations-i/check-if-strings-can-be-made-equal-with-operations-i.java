2be3dd110f3d147d2adf3a5770178f8305ee4477
kishore2006-kumar <kishorekumartr006@gmail.com>
2026-08-20 12:27:01 +0530

    Time: 1 ms (99.88%) | Memory: 44.9 MB (15.10%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,20 +1,9 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        char[] ch1 = s1.toCharArray();
-        char[] ch2 = s2.toCharArray();
+        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
+                ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+                        (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
 
-        int n = ch1.length;
-        for (int i = 0; i < n; i++) {
-            if (ch1[i] == ch2[i]) {
-                continue;
-            }
-            if (i + 2 < n && ch2[i + 2] == ch1[i]) {
-
-                char temp = ch2[i];
-                ch2[i] = ch2[i + 2];
-                ch2[i + 2] = temp;
-            }
-        }
-        return Arrays.equals(ch1, ch2);
     }
 }
