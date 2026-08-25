7a63a4095e2acb1c4326e69fdd330a89b91df645
kishore2006-kumar <kishorekumartr006@gmail.com>
2026-08-25 13:49:41 +0530

    Time: 1 ms (99.96%) | Memory: 46.2 MB (89.56%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        StringBuilder sb=new StringBuilder(magazine);
+        int[] freq=new int[26];
+        for(char ch : magazine.toCharArray()) {
+            freq[ch-'a']++;
+        }
         for(char ch : ransomNote.toCharArray()) {
-            String s = sb.toString();
-            if(s.indexOf(ch) != -1) {
-                sb.deleteCharAt(s.indexOf(ch));
-            }
-            else {
-                return false;
-            }
+            freq[ch-'a']--;
+        }
+        for(int i=0; i<26; i++) {
+            if(freq[i] < 0) return false;
         }
         return true;
     }
